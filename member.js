6d90938a51e107cc363f75fcc40df90a8a92aeaa
Vinaya Dasepalli <vinay.dasepalli@gmail.com>
2024-02-19 02:38:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMembers(){
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member:'-'
+        }
+    }
+}
